e69ca0ca7982bbb146a7e8f9ef4bfb7078e28e22
axtiwayguh/tasks <aziza200509@icloud.com>
2025-10-03 12:25:40 +0500

    Первый коммит

--- tasks.py
+++ tasks.py
@@ -1,11 +1,9 @@
-for (i=1,i<=50;i++) {
-    if (i%2==0) {
-        h=i++;
-        print(h);
-    }
-}
-p = int (input())
-for (i=p,i=1,i--;) {
-    l=i*i
-        print(l);
-    }
+for i in range(1, 51):
+    if i % 2 == 0:
+        h = i
+        print(h)
+
+p = int(input())
+for i in range(p, 0, -1):
+    l = i * i
+    print(l)
